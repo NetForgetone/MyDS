69d5ffd96f31d62f99fabefce814d8d2e79009ed
liuxiongwei <liu820211598@qq.com>
2021-05-22 17:20:17 +0800

    完善MAIN

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "sequence_list.h"
+
+int main()
+{
+    SqListInit();
+    return 0;
+}
